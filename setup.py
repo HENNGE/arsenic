6373d476c3a2ff4547a2d20305bc530127a4db26
Jonas Obrist <jonas.obrist@hde.co.jp>
2017-05-25 18:53:40 +0900

    Release 1.0.0.dev1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    version='0.1',
+    version='1.0.0.dev1',
     name='arsenic',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
