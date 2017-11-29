6472978e17657d94e71bcd14314558f991f9f7b0
Jonas Obrist <jonas.obrist@hde.co.jp>
2017-11-29 11:05:57 +0900

    1.0.0.dev7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    version='1.0.0.dev6',
+    version='1.0.0.dev7',
     name='arsenic',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
